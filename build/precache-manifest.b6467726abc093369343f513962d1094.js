5c93df35506f0ef285bb28af50b24486d3251de7
Rufat Aliyev <rufataliy@users.noreply.github.com>
2020-04-21 19:38:41 -0400

    about page text image added. font,

---
+++ build/precache-manifest.b6467726abc093369343f513962d1094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860460eeaabaf3f335e892bb94917866",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2d42e8d08d1d742feb7",
+    "url": "./static/js/2.2824bcf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2824bcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2dbb9336dd3a8f279d",
+    "url": "./static/js/main.07be958b.chunk.js"
+  },
+  {
+    "revision": "a062e1f0e1f924f559d3",
+    "url": "./static/js/runtime-main.2afa580b.js"
+  }
+]);
